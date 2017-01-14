fb49d0925d5f46767c1ef075d3d7c294a44e38a3
Maarten97 <m.s.looijenga@student.utwente.nl>
2017-01-14 19:54:34 +0100

    Mark with javadoc and JML

---
+++ Project2017/src/model/Mark.java
@@ -0,0 +1,25 @@
+package model;
+public enum Mark {
+    
+    EMPTY, XX, OO;
+
+    /*@
+       ensures this == Mark.XX ==> \result == Mark.OO;
+       ensures this == Mark.OO ==> \result == Mark.XX;
+       ensures this == Mark.EMPTY ==> \result == Mark.EMPTY;
+     */
+    /**
+     * Returns the other mark.
+     * 
+     * @return The other mark. If the Mark is Empty, Empty will be returned.
+     */
+    public Mark other() {
+        if (this == XX) {
+            return OO;
+        } else if (this == OO) {
+            return XX;
+        } else {
+            return EMPTY;
+        }
+    }
+}
