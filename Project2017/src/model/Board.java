8aa3849c1d8c534589bb46c52ed99f8e11d9913a
Maarten97 <m.s.looijenga@student.utwente.nl>
2017-01-14 20:11:50 +0100

    Start with the board of the game!

--- Project2017/src/model/Board.java
+++ Project2017/src/model/Board.java
@@ -1,9 +1,61 @@
 package model;
 
+
+
+/**
+ * Board of the 3D 4 in a row game.
+ * 
+ * @author Thomas Hogema en Maarten Looijenga
+ * @version 1.0
+ *
+ */
 public class Board {
 
+	public static final int DIM = 4;
+	
+	/**
+	 * The DIM by DIM by DIM fields of the game. 
+	 */
+	// @ private invariant fields.length == DIM*DIM*DIM;
+	/*
+	 * @ invariant (\forall int i; 0 <= i & i < DIM*DIM; getField(i) ==
+	 * Mark.EMPTY || getField(i) == Mark.XX || getField(i) == Mark.OO);
+	 */
+	private Mark[] fields;
+	
+	/**
+	 * Creates a list with the fields of a game.
+	 */
+	
+	// @ ensures (\forall int i; 0 <= i & i < DIM * DIM * DIM; this.getField(i) ==
+	// Mark.EMPTY);
 	public Board() {
-		// TODO Auto-generated constructor stub
+		fields = new Mark[DIM * DIM * DIM];
+		reset();
+	}
+	
+	/**
+	 * Creates a deep copy of this field.
+	 */
+	/*
+	 * @ ensures \result != this; ensures (\forall int i; 0 <= i & i < DIM *
+	 * DIM * DIM; \result.getField(i) == this.getField(i));
+	 * 
+	 */ 
+	public Board deepCopy() {
+		Board deepboard = new Board();
+		for (int i = 0; i < DIM * DIM * DIM; i++) {
+			deepboard.fields[i] = this.fields[i];
+		}
+		return deepboard;
+	}
+	
+	
+	
+	
+	private void reset() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
