f42e54e04ff5861241818eb942fa2c14389a4e0b
Maarten97 <m.s.looijenga@student.utwente.nl>
2017-01-24 13:30:46 +0100

    Made a main to play the game!

---
+++ Project2017/src/controller/GameMain.java
@@ -0,0 +1,21 @@
+package controller;
+
+import model.Mark;
+
+public class GameMain {
+
+	public GameMain() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Player player1;
+		Player player2;
+		player1 = new HumanPlayer("Thomas", Mark.BLUE);
+		player2 = new HumanPlayer("Thomas", Mark.BLUE);
+		Game game = new Game(player1, player2);
+		game.start();
+				
+	}
+
+}
